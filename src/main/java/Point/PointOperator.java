1d98e97f453a7352bee396a7a610dc7b346af740
me <loloplouc@gmail.com>
2024-01-27 17:07:31 -0500

    le reste

--- src/main/java/Point/PointOperator.java
+++ src/main/java/Point/PointOperator.java
@@ -11,7 +11,9 @@ public final class PointOperator {
      * @param translateVector Translation to apply
      */
     public static void translate(Double[] vector, Double[] translateVector) {
-
+        for(int i = 0; i < vector.length; i++) {
+            vector[i] = vector[i] + translateVector[i];
+        }
     }
 
     /** TODO
@@ -30,7 +32,9 @@ public final class PointOperator {
      * @param divider Scalar by which to divide
      */
     public static void divide(Double[] vector, Double divider) {
-
+        for(int i = 0; i < vector.length; i++){
+            vector[i] /= divider;
+        }
     }
 
     /** TODO
@@ -39,7 +43,9 @@ public final class PointOperator {
      * @param multiplier Scalar by which to multiply
      */
     public static void multiply(Double[] vector, Double multiplier) {
-
+        for(int i = 0; i < vector.length; i++){
+            vector[i] *= multiplier;
+        }
     }
 
     /** TODO
@@ -48,6 +54,8 @@ public final class PointOperator {
      * @param adder Scalar to add to vector
      */
     public static void add(Double[] vector, Double adder) {
-
+        for(int i = 0; i < vector.length; i++) {
+            vector[i] += adder;
+        }
     }
 }
